d26c4e36c82c4049a22d98201e947963a4c1361c
Dibakar Biswas <dibakar@gmail.com>
2013-12-18 23:09:50 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bepara::Application.config.secret_key_base = '4f98b45fe60b04ae57dee618d44758a411a40851c2c656618df5d716522685e19e3ba45a48daf6ff2fdaa2f5231cdba7f23685f9a323ceafedd0936b19ade6a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bepara::Application.config.session_store :cookie_store, key: '_bepara_session'
