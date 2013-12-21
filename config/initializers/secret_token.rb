c1f6eb68aa81487bf686653b16ed772d16342f5d
Dibakar Biswas <dibakar@gmail.com>
2013-12-21 15:58:50 -0500

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bepara::Application.config.secret_key_base = '4f98b45fe60b04ae57dee618d44758a411a40851c2c656618df5d716522685e19e3ba45a48daf6ff2fdaa2f5231cdba7f23685f9a323ceafedd0936b19ade6a0'
+#
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Bepara::Application.config.secret_key_base = secure_token
+
+# Bepara::Application.config.secret_key_base = '4f98b45fe60b04ae57dee618d44758a411a40851c2c656618df5d716522685e19e3ba45a48daf6ff2fdaa2f5231cdba7f23685f9a323ceafedd0936b19ade6a0'
